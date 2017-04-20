61a8702812927d224b9c9c69d303a6371f028780
Zaharchenko <o.zakharchenko@engagepoint.com>
2017-04-20 19:55:58 +0300

    Add newrelic

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['autobots'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '25e7e6b36a950f91aa5e1c3db7d8b64ea890a4c6',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
--- server/shared.js
+++ server/shared.js
@@ -2,6 +2,8 @@ import bodyParser from 'body-parser';
 import { askAllBots } from '../crawler/horn';
 import api from '../crawler/api';
 
+require('newrelic');
+
 const sharedMw = (app) => {
     const DELAY = 30000;
 
